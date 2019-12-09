67b480a5cd88a3c65b1efec56a070752541cb103
MJBackes <mjohnbackes@gmail.com>
2019-12-09 13:33:27 -0600

    Created VisualStudio Project

---
+++ TravelSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TravelSite.Startup))]
+namespace TravelSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
