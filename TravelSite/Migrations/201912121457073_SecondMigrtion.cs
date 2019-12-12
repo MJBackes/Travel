09a9cabd53af704efe4ef3d3a611365f7aa32dd8
nelsonju1897 <nelsonju1897@gmail.com>
2019-12-12 09:21:34 -0600

    Added ActionLink Buttons for Edit Interests, Edit Info, and Find Hotel.

---
+++ TravelSite/Migrations/201912121457073_SecondMigrtion.cs
@@ -0,0 +1,16 @@
+namespace TravelSite.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class SecondMigrtion : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
